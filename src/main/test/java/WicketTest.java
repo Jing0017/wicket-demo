f38c12bf9090ce2d98eb1874408225be1b98af1b
Yan Jing <564752135@qq.com>
2017-03-26 20:55:16 +0800

    add test

---
+++ src/main/test/java/WicketTest.java
@@ -0,0 +1,20 @@
+/**
+ * <p>
+ * Title :
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Author :严靖 26/03/2017
+ * </p>
+ * <p>
+ * Department :
+ * </p>
+ * Copyright :  </p>
+ */
+public class WicketTest {
+    public static void main(String[] args) {
+        
+    }
+}
